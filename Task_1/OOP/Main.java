d2328d4b7b6d5d702fdb535f04e4af9da09530d0
edblinova <greatner@yandex.ru>
2023-12-13 22:40:04 +0500

    Revert "Update Main.java"
    This reverts commit c9dff04ca408688f27270fb8dbd337967aa6df09.

--- Task_1/OOP/Main.java
+++ Task_1/OOP/Main.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-// 
+
 public class Main {
     public static void main(String[] args) {
         Product coffee = new Product("coffee", 150, 300);
